eec66b6bbcb21111580eea8a3865b75b961d7058
Reandy Ferdinanto <reandyferdinanto@gmail.com>
2024-05-12 18:44:40 +0700

    herklots portflio handRIP first commit

---
+++ src/section/about.jsx
@@ -0,0 +1,173 @@
+import React from 'react';
+import {
+  CircularProgressbar,
+  buildStyles
+} from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+
+import AnimatedProgressProvider from "../layout/AnimatedProgressProvider";
+
+import AOS from 'aos';
+AOS.init();
+
+
+const hero = () => {
+	return(
+		<div className="v-center">
+		<div className="container">
+			<div className="row">
+				<div className="col-md-12 text-center">
+	                <h2 data-aos="fade-up"
+                        data-aos-delay="0"
+                        data-aos-duration="1000"
+                        data-aos-easing="ease"
+                        data-aos-once="true">About Me</h2>
+	                <div className="space-border"
+	                	data-aos="fade-up"
+                        data-aos-delay="20"
+                        data-aos-duration="1000"
+                        data-aos-easing="ease"
+                        data-aos-once="true"
+	                	></div>
+	            </div>
+	            <div className="col-md-8 text-center m-auto"
+	            	data-aos="fade-up"
+                    data-aos-delay="60"
+                    data-aos-duration="1000"
+                    data-aos-easing="ease"
+                    data-aos-once="true"
+	            	>
+					<p>Graphic Designer with 2 years of experience in the industry</p>
+	            	<p>My work has spanned to 3D artist, Video Editing, and Videography. My passion is to make things better
+              		efficiently and as fast as possible with little mistakes.
+                     </p>
+	            </div>
+			</div>
+			<div className="row">
+				<div className="col-lg-3 p-5 text-center"
+					data-aos="fade"
+                    data-aos-delay="300"
+                    data-aos-duration="1000"
+                    data-aos-easing="ease"
+                    data-aos-once="true"
+					>
+                    <AnimatedProgressProvider
+				        valueStart={0}
+				        valueEnd={90}>
+				        {value => {
+				          const roundedValue = Math.round(value);
+				          return (
+				            <CircularProgressbar
+				              value={value}
+				              strokeWidth={3}
+				              text={`${roundedValue}.0%`}
+				              styles={buildStyles({
+				              		pathTransition: "none",
+							          textColor: "white",
+							          textSize: "8px",
+							          pathColor: "#cf1f1f",
+							          trailColor: "#2f3134"
+							        })}
+											            />
+				          );
+				        }}
+				      </AnimatedProgressProvider>
+				      <h4 className="mt-2">Blender</h4>
+                </div>
+                <div className="col-lg-3 p-5 text-center"
+                	data-aos="fade"
+                    data-aos-delay="400"
+                    data-aos-duration="1000"
+                    data-aos-easing="ease"
+                    data-aos-once="true"
+                	>
+                    <AnimatedProgressProvider
+				        valueStart={0}
+				        valueEnd={85}>
+				        {value => {
+				          const roundedValue = Math.round(value);
+				          return (
+				            <CircularProgressbar
+				              value={value}
+				              strokeWidth={3}
+				              text={`${roundedValue}.0%`}
+				              styles={buildStyles({
+				              		pathTransition: "none",
+							          textColor: "white",
+							          textSize: "8px",
+							          pathColor: "#cf1f1f",
+							          trailColor: "#2f3134"
+							        })}
+											            />
+				          );
+				        }}
+				      </AnimatedProgressProvider>
+				      <h4 className="mt-2">Adobe</h4>
+                </div>
+                <div className="col-lg-3 p-5 text-center"
+                	data-aos="fade"
+                    data-aos-delay="500"
+                    data-aos-duration="1000"
+                    data-aos-easing="ease"
+                    data-aos-once="true"
+                	>
+                    <AnimatedProgressProvider
+				        valueStart={0}
+				        valueEnd={80}>
+				        {value => {
+				          const roundedValue = Math.round(value);
+				          return (
+				            <CircularProgressbar
+				              value={value}
+				              strokeWidth={3}
+				              text={`${roundedValue}.0%`}
+				              styles={buildStyles({
+				              		pathTransition: "none",
+							          textColor: "white",
+							          textSize: "8px",
+							          pathColor: "#cf1f1f",
+							          trailColor: "#2f3134"
+							        })}
+											            />
+				          );
+				        }}
+				      </AnimatedProgressProvider>
+				      <h4 className="mt-2">Canva</h4>
+                </div>
+                <div className="col-lg-3 p-5 text-center"
+                	data-aos="fade"
+                    data-aos-delay="600"
+                    data-aos-duration="1000"
+                    data-aos-easing="ease"
+                    data-aos-once="true"
+                	>
+                    <AnimatedProgressProvider
+				        valueStart={0}
+				        valueEnd={75}>
+				        {value => {
+				          const roundedValue = Math.round(value);
+				          return (
+				            <CircularProgressbar
+				              value={value}
+				              strokeWidth={3}
+				              text={`${roundedValue}.0%`}
+				              styles={buildStyles({
+				              		pathTransition: "none",
+							          textColor: "white",
+							          textSize: "8px",
+							          pathColor: "#cf1f1f",
+							          trailColor: "#2f3134"
+							        })}
+											            />
+				          );
+				        }}
+				      </AnimatedProgressProvider>
+				      <h4 className="mt-2">Video Editor</h4>
+                </div>
+			</div>
+		</div>
+		</div>
+	);
+}
+
+export default hero;
