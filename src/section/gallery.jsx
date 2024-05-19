f403ebb5442ee57f04258898a27393d875a6fdba
Reandy Ferdinanto <reandyferdinanto@gmail.com>
2024-05-19 22:37:55 +0700

    feat: update portfolio content

--- src/section/gallery.jsx
+++ src/section/gallery.jsx
@@ -50,6 +50,8 @@ const Gallery= function() {
       var x = document.getElementsByTagName("BODY")[0];
       x.style.overflow = "auto";
     };
+
+
     return(
             <div className="container">
             <GlobalStyles/>
@@ -67,10 +69,10 @@ const Gallery= function() {
                         >
                         <div className="card-image-1">
                             <div className="d-text">
-                                <h3>Archi Interior Design</h3>
-                                <h5 className="d-tag">website</h5>
+                                <h3>WEALTH DON'T MATTER HERE, 2022</h3>
+                                <h5 className="d-tag">3D model</h5>
                             </div>
-                            <video  src="./img/gallery/ineke-hans-pf.mp4" autoPlay loop muted playsInline preload="auto" alt="portfolio-1" style={{ height: '250px' }}/>
+                            <img  src="./img/gallery/wealth-pf.jpg" alt="portfolio-1" style={{ height: '500px' }}/>
                         </div>
                     </div>
                     <div className="col-lg-4 image-element-class de_modal" onClick={handleBtnClick1}
@@ -79,7 +81,7 @@ const Gallery= function() {
                         > 
                         <div className="card-image-1">
                             <div className="d-text">
-                                <h3>My beautiful flower, 20</h3>
+                                <h3>My beautiful flower, 2022</h3>
                                 <h5 className="d-tag">website</h5>
                             </div>
                             <img src="./img/gallery/my-pf.jpg"  alt="My beautiful flower, 20"/>
@@ -94,7 +96,7 @@ const Gallery= function() {
                                 <h3>Jakarta Town Festival</h3>
                                 <h5 className="d-tag">website</h5>
                             </div>
-                            <video src="./img/gallery/jakarta-pf.mp4" autoPlay loop muted playsInline preload="auto" alt="portfolio-1" style={{ height: '500px' }}/>
+                            <img src="./img/gallery/jakarta-pf.gif" alt="portfolio-1" style={{ height: '700px' }}/>
                         </div>
                     </div>
                     <div className="col-lg-4 image-element-class de_modal" onClick={handleBtnClick}
@@ -103,10 +105,10 @@ const Gallery= function() {
                         >
                         <div className="card-image-1">
                             <div className="d-text">
-                                <h3>Bluetec App</h3>
-                                <h5 className="d-tag">website</h5>
+                                <h3>TO SEEK LIFE, JULY 2022</h3>
+                                <h5 className="d-tag">3D model</h5>
                             </div>
-                            <img src="./img/gallery/4.jpg" alt="gallery"/>
+                            <img src="./img/gallery/to-seek-pf.jpg" alt="gallery"/>
                         </div>
                     </div>
                     <div className="col-lg-4 image-element-class de_modal" onClick={handleBtnClick1}
@@ -115,10 +117,10 @@ const Gallery= function() {
                         >
                         <div className="card-image-1">
                             <div className="d-text">
-                                <h3>Uhost Hosting</h3>
-                                <h5 className="d-tag">website</h5>
+                                <h3>007 NO TIME TO DIE OPENING TITLE REDESIGN, 2022</h3>
+                                <h5 className="d-tag">3D Animation</h5>
                             </div>
-                            <img src="./img/gallery/5.jpg" alt="gallery"/>
+                            <img src="./img/gallery/007-pf.gif" alt="portfolio-2" style={{ height: '200px' }}/>
                         </div>
                     </div>
                     <div className="col-lg-4 image-element-class de_modal" onClick={handleBtnClick2}
@@ -127,10 +129,22 @@ const Gallery= function() {
                         >
                         <div className="card-image-1">
                             <div className="d-text">
-                                <h3>Bolo Creative</h3>
-                                <h5 className="d-tag">website</h5>
+                                <h3>Ineke Hans recycleable chair video explainer, 2022</h3>
+                                <h5 className="d-tag">3D Animation</h5>
                             </div>
-                            <img src="./img/gallery/6.jpg" alt="gallery"/>
+                            <img src="./img/gallery/ineke-hans-pf.gif" alt="gallery"/>
+                        </div>
+                    </div>
+                    <div className="col-lg-4 image-element-class de_modal" onClick={handleBtnClick2}
+                        data-aos="fade-up"
+                        data-aos-once="true"
+                        >
+                        <div className="card-image-1">
+                            <div className="d-text">
+                                <h3>PLAZA DESAIN EMOTIA TEASER TRAILER, 2022</h3>
+                                <h5 className="d-tag">video effect</h5>
+                            </div>
+                            <img src="./img/gallery/plaza-pf.gif" alt="gallery"/>
                         </div>
                     </div>
                   </Masonry>
@@ -145,25 +159,25 @@ const Gallery= function() {
                       <div className="mainLightbox container">
                         <div className="row g-5">
                             <div className="col-lg-8">
-                                <img src="./img/gallery-big/1.jpg" alt="" className="img-fluid"/>
+                                <img src="./img/gallery/wealth-pf.jpg" alt="" className="img-fluid" style={{width:"750px"}}/>
                             </div>
 
                             <div className="col-lg-4 de_project-info">
-                                <h3>Interior Design Website</h3>
-                                <p>Duis ad adipisicing id id in duis nisi qui veniam esse voluptate cillum pariatur cupidatat ut dolore amet aliquip cillum ad minim cillum nulla consectetur dolor culpa deserunt mollit dolor ea pariatur laboris sed tempor laboris quis commodo. Ut duis cupidatat consectetur cillum veniam eu quis minim irure fugiat ut consequat do veniam duis dolor quis cupidatat deserunt ut qui minim id dolor nisi sed non ut proident enim culpa dolor elit dolor aliquip dolore enim excepteur.</p>
+                                <h3>WEALTH DON'T MATTER HERE, 2022</h3>
+                                <p>An Assignment with the theme of wealth where no matter what your status is, we all live and die in the same land and dirt.</p>
                                
                                 <div className="de_project-details">
                                     <div className="d-field">
-                                        <i className="fa fa-user-o"></i>Client: <span>Envato</span>
+                                        <i className="fa fa-user-o"></i>Client: <span>Portfolio</span>
                                     </div>
                                     <div className="d-field">
-                                        <i className="fa fa-file-text-o"></i>Type: <span>Branding</span>
+                                        <i className="fa fa-file-text-o"></i>Type: <span>3D Modeling</span>
                                     </div>
                                     <div className="d-field">
                                         <i className="fa fa-calendar-o"></i>Year: <span>2020</span>
                                     </div>
                                     <div className="d-field">
-                                        <i className="fa fa-external-link"></i>Preview: <span><a href="https://www.envato.com" target="_blank" rel="noreferrer">www.envato.com</a></span>
+                                        <i className="fa fa-external-link"></i>Preview: <span><a href="https://" target="_blank" rel="noreferrer">-</a></span>
                                     </div>
                                 </div>
                                 <div className="spacer-30"></div>
@@ -190,9 +204,9 @@ const Gallery= function() {
                             <div className="col-lg-8">
                                 <div className="row g-4">
                                     <div className="col-lg-12 item">
-                                        <img src="./img/single-gallery/pf-2-1.jpg" alt="galleryimage" className="img-fluid"/>
+                                        <img src="./img/gallery/my-pf.jpg" alt="galleryimage" className="img-fluid"/>
                                     </div>
-                                    <div className="col-lg-6 item">
+                                    {/* <div className="col-lg-6 item">
                                         <img src="./img/single-gallery/pf-2-2.jpg" alt="galleryimage" className="img-fluid"/>
                                     </div>
                                     <div className="col-lg-6 item">
@@ -206,32 +220,32 @@ const Gallery= function() {
                                     </div>
                                     <div className="col-lg-6 item">
                                         <img src="./img/single-gallery/pf-2-6.jpg" alt="galleryimage" className="img-fluid"/>
-                                    </div>
+                                    </div> */}
                                 </div>
                             </div>
 
                             <div className="col-lg-4 de_project-info">
-                                <h3>Interior Design Website</h3>
-                                <p>Duis ad adipisicing id id in duis nisi qui veniam esse voluptate cillum pariatur cupidatat ut dolore amet aliquip cillum ad minim cillum nulla consectetur dolor culpa deserunt mollit dolor ea pariatur laboris sed tempor laboris quis commodo. Ut duis cupidatat consectetur cillum veniam eu quis minim irure fugiat ut consequat do veniam duis dolor quis cupidatat deserunt ut qui minim id dolor nisi sed non ut proident enim culpa dolor elit dolor aliquip dolore enim excepteur.</p>
+                                <h3>My beautiful flower, 2022</h3>
+                                <p>A personal project representing the beautiful side of nature, a study on Armature rig</p>
                                
                                 <div className="de_project-details">
                                     <div className="d-field">
-                                        <i className="fa fa-user-o"></i>Client: <span>Envato</span>
+                                        <i className="fa fa-user-o"></i>Client: <span>Portfolio</span>
                                     </div>
                                     <div className="d-field">
-                                        <i className="fa fa-file-text-o"></i>Type: <span>Branding</span>
+                                        <i className="fa fa-file-text-o"></i>Type: <span>3D Modeling</span>
                                     </div>
                                     <div className="d-field">
-                                        <i className="fa fa-calendar-o"></i>Year: <span>2020</span>
+                                        <i className="fa fa-calendar-o"></i>Year: <span>2022</span>
                                     </div>
                                     <div className="d-field">
-                                        <i className="fa fa-external-link"></i>Preview: <span><a href="https://www.envato.com" target="_blank" rel="noreferrer">www.envato.com</a></span>
+                                        <i className="fa fa-external-link"></i>Preview: <span><a href="https://" target="_blank" rel="noreferrer">-</a></span>
                                     </div>
                                 </div>
                                 <div className="spacer-30"></div>
                                 <blockquote>
-                                    "Very well done theme. Versatile, extremely well coded, and gorgeous. That's a great combo. Highly recommended."
-                                    <span>John Archi</span>
+                                    "An Awesome Sun Flower"
+                                    <span>Herklots Harold</span>
                                 </blockquote>
                             </div>
                         </div>
