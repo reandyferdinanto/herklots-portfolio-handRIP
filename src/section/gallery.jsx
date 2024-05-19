73aab02e37d15c047618b2f463f89d6539238f78
Reandy Ferdinanto <reandyferdinanto@gmail.com>
2024-05-19 22:53:13 +0700

    feat: update portfolio content

--- src/section/gallery.jsx
+++ src/section/gallery.jsx
@@ -51,6 +51,19 @@ const Gallery= function() {
       x.style.overflow = "auto";
     };
 
+    /* lightbox3 */
+    const [lighbx3, setlighbx3] = React.useState(false);
+    const handleBtnClick3 = () => {
+        setlighbx3(!lighbx3);
+        var x = document.getElementsByTagName("BODY")[0];
+        x.style.overflow = "hidden";
+    };
+    const handleBtnClickclose3 = () => {
+        setlighbx2(!lighbx2);
+        var x = document.getElementsByTagName("BODY")[0];
+        x.style.overflow = "auto";
+    };
+
 
     return(
             <div className="container">
@@ -111,7 +124,7 @@ const Gallery= function() {
                             <img src="./img/gallery/to-seek-pf.jpg" alt="gallery"/>
                         </div>
                     </div>
-                    <div className="col-lg-4 image-element-class de_modal" onClick={handleBtnClick1}
+                    <div className="col-lg-4 image-element-class de_modal" onClick={handleBtnClick3}
                         data-aos="fade-up"
                         data-aos-once="true"
                         >
@@ -120,10 +133,10 @@ const Gallery= function() {
                                 <h3>007 NO TIME TO DIE OPENING TITLE REDESIGN, 2022</h3>
                                 <h5 className="d-tag">3D Animation</h5>
                             </div>
-                            <img src="./img/gallery/007-pf.gif" alt="portfolio-2" style={{ height: '200px' }}/>
+                            <img src="./img/gallery/007-pf.gif" alt="portfolio-2" style={{ width: '400px' }}/>
                         </div>
                     </div>
-                    <div className="col-lg-4 image-element-class de_modal" onClick={handleBtnClick2}
+                    <div className="col-lg-4 image-element-class de_modal" onClick={handleBtnClick}
                         data-aos="fade-up"
                         data-aos-once="true"
                         >
@@ -258,40 +271,85 @@ const Gallery= function() {
                 {lighbx2 && ( 
                   <div className="LightboxGal">
                     <div className="closeGal">
-                        <button className="button-close" onClick={handleBtnClickclose2}></button>
+                        <button className="button-close" onClick={handleBtnClickclose3}></button>
                     </div>
                     <div className="v-center w-100">
                       <div className="mainLightbox container">
                         <div className="row g-5">
                             <div className="col-lg-8">
-                                <video className="pop" controls autoPlay loop>
-                                  <source src="./video/local-video-2.mp4" type="video/mp4"/>
+                                <video className="pop" autoPlay loop  style={{width:"700px"}}>
+                                  <source src="./img/gallery/jakarta-pf.mp4" type="video/mp4"/>
                                   Your browser does not support the video tag.
                                 </video>
                             </div>
 
                             <div className="col-lg-4 de_project-info">
-                                <h3>Self Hosted Video</h3>
-                                <p>Duis ad adipisicing id id in duis nisi qui veniam esse voluptate cillum pariatur cupidatat ut dolore amet aliquip cillum ad minim cillum nulla consectetur dolor culpa deserunt mollit dolor ea pariatur laboris sed tempor laboris quis commodo. Ut duis cupidatat consectetur cillum veniam eu quis minim irure fugiat ut consequat do veniam duis dolor quis cupidatat deserunt ut qui minim id dolor nisi sed non ut proident enim culpa dolor elit dolor aliquip dolore enim excepteur.</p>
+                                <h3>JAKARTA TOWN FESTIVAL TEASER ANIMATION, 2022</h3>
+                                <p>Teaser trailer for Jakarta Town Festival using Blender and After Effects with assets given from Art Director</p>
                                
                                 <div className="de_project-details">
                                     <div className="d-field">
-                                        <i className="fa fa-user-o"></i>Client: <span>Envato</span>
+                                        <i className="fa fa-user-o"></i>Client: <span>Portfolio</span>
                                     </div>
                                     <div className="d-field">
-                                        <i className="fa fa-file-text-o"></i>Type: <span>Branding</span>
+                                        <i className="fa fa-file-text-o"></i>Type: <span>Video Animation</span>
                                     </div>
                                     <div className="d-field">
-                                        <i className="fa fa-calendar-o"></i>Year: <span>2020</span>
+                                        <i className="fa fa-calendar-o"></i>Year: <span>2022</span>
                                     </div>
                                     <div className="d-field">
-                                        <i className="fa fa-external-link"></i>Preview: <span><a href="https://www.envato.com" target="_blank" rel="noreferrer">www.envato.com</a></span>
+                                        <i className="fa fa-external-link"></i>Preview: <span><a href="https://" target="_blank" rel="noreferrer">-</a></span>
                                     </div>
                                 </div>
                                 <div className="spacer-30"></div>
                                 <blockquote>
-                                    "Very well done theme. Versatile, extremely well coded, and gorgeous. That's a great combo. Highly recommended."
-                                    <span>John Archi</span>
+                                    Time spent - 5 Hours
+                                    <span>Herklots Harold</span>
+                                </blockquote>
+                            </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  )}
+
+                {lighbx3 && ( 
+                  <div className="LightboxGal">
+                    <div className="closeGal">
+                        <button className="button-close" onClick={handleBtnClickclose3}></button>
+                    </div>
+                    <div className="v-center w-100">
+                      <div className="mainLightbox container">
+                        <div className="row g-5">
+                            <div className="col-lg-8">
+                                <video className="pop" autoPlay loop  style={{width:"800px"}}>
+                                  <source src="./img/gallery/007-pf.mp4" type="video/mp4"/>
+                                  Your browser does not support the video tag.
+                                </video>
+                            </div>
+
+                            <div className="col-lg-4 de_project-info">
+                                <h3>07 NO TIME TO DIE OPENING TITLE REDESIGN, 2022</h3>
+                                <p>Portraits of creatives in their home studios and local environs.</p>
+                               
+                                <div className="de_project-details">
+                                    <div className="d-field">
+                                        <i className="fa fa-user-o"></i>Client: <span>Portfolio</span>
+                                    </div>
+                                    <div className="d-field">
+                                        <i className="fa fa-file-text-o"></i>Type: <span>Video Animation</span>
+                                    </div>
+                                    <div className="d-field">
+                                        <i className="fa fa-calendar-o"></i>Year: <span>2022</span>
+                                    </div>
+                                    <div className="d-field">
+                                        <i className="fa fa-external-link"></i>Preview: <span><a href="https://" target="_blank" rel="noreferrer">-</a></span>
+                                    </div>
+                                </div>
+                                <div className="spacer-30"></div>
+                                <blockquote>
+                                    Time spent - 2 weeks
+                                    <span>Herklots Harold</span>
                                 </blockquote>
                             </div>
                         </div>
